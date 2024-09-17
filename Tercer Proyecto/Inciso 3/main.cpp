b0b82764f4aee0c3356ea5ea206a95c529bd558c
BGPS1996 <grabixz.29@gmail.com>
2024-09-17 20:19:43 -0300

    Rename Folders

---
+++ Tercer Proyecto/Inciso 3/main.cpp
@@ -0,0 +1,15 @@
+
+/*
+3.- Implementar un algoritmo de backtracking para resolver un laberinto descripto como una matriz de nxn de bool en la que las posiciones 
+marcadas como false son interpretadas como pared.
+Nota: No es necesario utilizar la clase matriz implementada previamente, sino que puede usarse una estructura más simple pero el 
+tablero debe ser arbitrario y el algoritmo debe ser paramétrico en este. */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+	
+	return 0;
+}
